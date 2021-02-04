ac873a800824923832f3a26195076a30399f21c5
unknown <fahim.ferdous64@gmail.com>
2021-02-04 19:01:35 +0600

    First commit

---
+++ Registration.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Web Technologies </title>
+</head>
+<body>
+<h1>REGISTRATION </h1>
+
+<form>
+  <label>Name           :</label>
+  <input type="text" id="fname" name="fname"><br><br>
+
+  <label>Email          :</label>
+  <input type="text" id="lname" name="lname"><br><br>
+
+  <label>User name         :</label>
+  <input type="text"><br><br>
+
+  <label>Password         :</label>
+  <input type="text"><br><br>
+
+  <label>Confirm Password         :</label>
+  <input type="text"><br><br>
+
+  <label>Gender         :</label><br><br>
+  <input type="radio" id="male" name="gender" value="Male">
+  <label for="Male">Male</label>
+  <input type="radio" id="female" name="gender" value="Female">
+  <label for="Female">Female</label>
+  <input type="radio" id="other" name="gender" value="Other">
+  <label for="Other">Other</label><br><br>
+
+  <label>Date of Birth</label><br><br>
+  <input type="date"><br><br>
+
+  <input type="submit" value = "SUBMIT"> 
+  <input type="reset" value = "RESET">
+
+</form>
+
+</body>
+</html>
---
+++ excercise.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Web Technologies </title>
+</head>
+My 2nd Task Of Web Technologies
+</html>
